cfca9f6ad3f521b89999bd47b1113b78e70fe48f
Merric Evangelista <ramdac17@gmail.com>
2026-03-13 20:40:47 +0800

    CHORE: Updated Post Event for Cloudinary

--- apps/api/src/articles/articles.module.ts
+++ apps/api/src/articles/articles.module.ts
@@ -2,7 +2,7 @@ import { Module, forwardRef } from '@nestjs/common';
 import { ArticlesController } from './articles.controller.js';
 import { ArticlesService } from './articles.service.js';
 import { PrismaService } from '../prisma.service.js';
-import { AuthModule } from 'src/auth/auth.module.js';
+import { AuthModule } from '../auth/auth.module.js';
 
 @Module({
   imports: [forwardRef(() => AuthModule)],
