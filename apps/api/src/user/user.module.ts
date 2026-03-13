53aedfd68aa42ca46f5c9c81d106b343a91171b2
Merric Evangelista <ramdac17@gmail.com>
2026-03-13 18:04:32 +0800

    CHORE: Updated image upload to Cloudinary

--- apps/api/src/user/user.module.ts
+++ apps/api/src/user/user.module.ts
@@ -2,13 +2,14 @@ import { Module } from '@nestjs/common';
 import { UserService } from './user.service.js';
 import { UserController } from './user.controller.js';
 import { PrismaService } from '../prisma.service.js';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule],
   controllers: [UserController],
   providers: [
-    UserService, 
-    PrismaService // ✅ Injecting the service we've been fixing
+    UserService,
+    PrismaService, // ✅ Injecting the service we've been fixing
   ],
   exports: [UserService], // Exporting if other modules need to find users
 })
