60833a415285c0b81239022debd9c3ddcd9e49cb
Merric Evangelista <ramdac17@gmail.com>
2026-04-16 00:21:19 +0800

    CHORE: Updated Cloudinary Module

--- apps/api/src/activities/activities.module.ts
+++ apps/api/src/activities/activities.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { ActivitiesController } from './activities.controller';
 import { ActivitiesService } from './activities.service';
 import { PrismaModule } from '../prisma.module';
-import { ShareCardModule } from '@/share-card/share-card.module';
+import { ShareCardModule } from '../share-card/share-card.module';
 
 @Module({
   imports: [PrismaModule, ShareCardModule],
