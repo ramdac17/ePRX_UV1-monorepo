d117ef525014b5278e2296f334eae2069b3b3de9
Merric Evangelista <ramdac17@gmail.com>
2026-05-21 17:48:38 +0800

    CHORE: Updated Leaderboard imports

--- apps/api/src/leaderBoard/leaderboard.module.ts
+++ apps/api/src/leaderBoard/leaderboard.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
-import { LeaderboardController } from './../leaderboard/leaderboard.controller';
-import { LeaderboardService } from './../leaderboard/leaderboard.service';
+import { LeaderboardController } from './leaderboard.controller';
+import { LeaderboardService } from './leaderboard.service';
 import { PrismaService } from '../prisma.service'; // Adjust path to your PrismaService
 
 @Module({
